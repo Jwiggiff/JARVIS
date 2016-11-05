b376377c337a6bee16b2ca8ed2fc1bfe85a9e494
Jwiggiff <jcool.friedman@gmail.com>
2016-11-05 13:18:30 -0400

    update website

--- JARVIS_files/navigation.js
+++ JARVIS_files/navigation.js
@@ -100,7 +100,7 @@ function navTab(name) {
 }
 $(window).resize(reloadSlider);
 $(document).ready(function() {
-  console.log('yup');
+  console.log('wasdwasd');
   var state = Navigate.getState();
   var path = window.location.pathname;
   var page = path.split("/")[1];
