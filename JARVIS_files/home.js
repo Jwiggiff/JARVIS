c906ae37f87540950f540e4f215680e07de2a93c
Jwiggiff <jcool.friedman@gmail.com>
2016-11-06 11:41:30 -0500

    update website

--- JARVIS_files/home.js
+++ JARVIS_files/home.js
@@ -44,6 +44,7 @@ function updateServers() {
   xmlhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
           var myArr = JSON.parse(this.responseText);
+          console.log(myArr);
           myFunction(myArr);
       }
   };
