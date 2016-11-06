e4abe633cabc8154354e60f4a2339e2c375995d6
Jwiggiff <jcool.friedman@gmail.com>
2016-11-06 11:48:34 -0500

    update website

--- JARVIS_files/navigation.js
+++ JARVIS_files/navigation.js
@@ -124,15 +124,4 @@ $(document).ready(function() {
   tabs.click(function() {
     loadTab($(this), true);
   });
-  $.ajax({
-    url: "https://bots.discord.pw/api/bots/236949446091472896/stats",
-    headers: {
-        'Authorization':'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySUQiOiIxNzQyOTE1NTg0NDk4MDczNjEiLCJyYW5kIjoyMTYsImlhdCI6MTQ3NjY1MDY2NX0.z3krQHAXxpFdinKEiD5sZWed50U6ZEyz8DWMIhLLUEg',
-    },
-    method: 'GET',
-    dataType: 'json',
-    success: function(data){
-      console.log(data);
-    }
-  });
 });
