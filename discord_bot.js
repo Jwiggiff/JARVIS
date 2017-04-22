3b35e33e5c3525dd2ce5a760b0bc3964d190535c
Jwiggiff <Jcool.friedman@gmail.com>
2017-04-22 16:20:32 -0400

    Update discord_bot.js

--- discord_bot.js
+++ discord_bot.js
@@ -762,7 +762,7 @@ bot.on('ready', () => {
 	    fs.writeFile('./serverconfigs.json', JSON.stringify(servers));
 	  }
 	}
-	console.log(servers["JARVIS Official Server"].antibully);
+	//console.log(servers["JARVIS Official Server"].antibully);
 	bot.user.setAvatar('http://i.imgur.com/l2KqI3Y.png?1');
 	package = require('./package.json');
   console.log("Starting " + package.name + " " + package.version + "...\nLogged in! Serving in " + bot.guilds.array().length + " servers");
